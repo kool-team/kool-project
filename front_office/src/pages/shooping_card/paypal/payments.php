d13f047b7ea593b6945124688be0ab3178e04fa2
m-o-d-e <101446136+m-o-d-e@users.noreply.github.com>
2022-05-29 21:01:25 +0100

    changes 2.1

---
+++ front_office/src/pages/shooping_card/paypal/payments.php
@@ -0,0 +1,125 @@
+<?php
+//check.php
+
+session_start();
+$con = new mysqli('localhost','root','');
+$dbconfig = mysqli_select_db($con,'kool_db');
+$user_id=$_SESSION['id'];
+$total=$_SESSION['total'];
+$session_id=$_SESSION['session_id'];
+if(isset($_POST['checkout'])){
+    $query="INSERT INTO order_details(user_id,total) values ($user_id,$total)";
+    $result=mysqli_query($con,$query);
+    $query="SELECT id FROM order_details ORDER BY id DESC LIMIT 1";
+    $result=mysqli_query($con,$query);
+    $row=mysqli_fetch_array($result);
+    $id=$row['id'];
+    $query="SELECT * from cart_item where session_id=$session_id" ;
+    $result=mysqli_query($con,$query);
+    while( $row=mysqli_fetch_assoc($result)){ 
+    $product_id=$row['product_id'];  
+    $query="INSERT INTO order_items(order_id,product_id) values ($id,$product_id)";
+    $result1=mysqli_query($con,$query);
+    }
+
+    header('location:../../../indexprofil.php');    
+}
+
+//*****************************Sending Request**********************************
+
+ // For test payments we want to enable the sandbox mode. If you want to put live
+// payments through then this setting needs changing to `false`.
+$enableSandbox = true;
+
+// Database settings. Change these for your database configuration.
+$dbConfig = [
+    'host' => 'localhost',
+    'email' => 'root',
+    'password' => 'root',
+    'name' => 'kool'
+];
+
+// PayPal settings. Change these to your account details and the relevant URLs
+// for your site.
+$paypalConfig = [
+    'email' => 'kool@gmail.com',
+    'return_url' => 'http://example.com/payment-successful.html',
+    'cancel_url' => 'http://example.com/payment-cancelled.html',
+    'notify_url' => 'http://example.com/payments.php'
+];
+
+$paypalUrl = $enableSandbox ? 'https://www.sandbox.paypal.com/cgi-bin/webscr' : 'https://www.paypal.com/cgi-bin/webscr';
+
+// Product being purchased.
+$itemName = 'PIZZA HUT';
+$itemAmount = 10.00;
+
+// Include Functions
+require 'functions.php';
+
+// Check if paypal request or response
+if (!isset($_POST["txn_id"]) && !isset($_POST["txn_type"])) {
+
+    // Grab the post data so that we can set up the query string for PayPal.
+    // Ideally we'd use a whitelist here to check nothing is being injected into
+    // our post data.
+    $data = [];
+    foreach ($_POST as $key => $value) {
+        $data[$key] = stripslashes($value);
+    }
+
+    // Set the PayPal account.
+    $data['business'] = $paypalConfig['email'];
+
+    // Set the PayPal return addresses.
+    $data['return'] = stripslashes($paypalConfig['return_url']);
+    $data['cancel_return'] = stripslashes($paypalConfig['cancel_url']);
+    $data['notify_url'] = stripslashes($paypalConfig['notify_url']);
+
+    // Set the details about the product being purchased, including the amount
+    // and currency so that these aren't overridden by the form data.
+    $data['item_name'] = $itemName;
+    $data['amount'] = $itemAmount;
+    $data['currency_code'] = 'USD';
+
+    // Add any custom fields for the query string.
+    //$data['custom'] = USERID;
+
+    // Build the query string from the data.
+    $queryString = http_build_query($data);
+
+    // Redirect to paypal IPN
+    header('location:' . $paypalUrl . '?' . $queryString);
+    exit();
+
+} else {
+    // Handle the PayPal response.
+
+    // Create a connection to the database.
+    $db = new mysqli($dbConfig['host'], $dbConfig['email'], $dbConfig['password'], $dbConfig['name']);
+
+    // Assign posted variables to local data array.
+    $data = [
+        'item_name' => $_POST['item_name'],
+        'item_number' => $_POST['item_number'],
+        'payment_status' => $_POST['payment_status'],
+        'payment_amount' => $_POST['mc_gross'],
+        'payment_currency' => $_POST['mc_currency'],
+        'txn_id' => $_POST['txn_id'],
+        'receiver_email' => $_POST['receiver_email'],
+        'payer_email' => $_POST['payer_email'],
+        'custom' => $_POST['custom'],
+    ];
+
+    // We need to verify the transaction comes from PayPal and check we've not
+    // already processed the transaction before adding the payment to our
+    // database.
+    if (verifyTransaction($_POST) && checkTxnid($data['txn_id'])) {
+        if (addPayment($data) !== false) {
+            // Payment successfully added.
+        }
+    }
+}
+
+
+?>
